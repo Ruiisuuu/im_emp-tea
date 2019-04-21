1d0f62561ec710430a1d9a9b32a06299eb488cdd
Louis Mollick <louismollick@gmail.com>
2019-04-21 02:54:50 -0400

    working lobby, main game

--- client/client.js
+++ client/client.js
@@ -6,6 +6,24 @@
 */
 
 (function() { // module pattern
+    const SIZE   = { tw: 30, th: 30},
+        INV_SIZE = { th: 2, tbutton: 5, ttimer: 7},
+        TILE     = 20,
+        COLOR    = { WHITE: '#ffffff',BLACK: '#000000', YELLOW: '#ECD078', BRICK: '#D95B43', PINK: '#C02942', PURPLE: '#542437', GREY: '#333', SLATE: '#53777A', GOLD: 'gold', GREEN: '#26A65B'},
+        KEY      = { ESC: 27, R: 82, W: 87, A: 65, D: 68, R: 82, SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39 },
+        BLOCKS   = { NULL: 0, SPAWN: 1, GOAL: 2, BEDROCK: 3,  BRICK: 4, WOOD: 5 },
+        MOUSE_OFFSET = {x: -TILE/2, y: -TILE/2};
+    
+    let width    = SIZE.tw * TILE;
+    let height   = SIZE.th * TILE;
+    let canvas1   = document.getElementById('canvas1');
+    let ctx1      = canvas1.getContext('2d');
+    let canvas2   = document.getElementById('canvas2');
+    let ctx2      = canvas2.getContext('2d');
+    
+    canvas1.width  = canvas2.width = width;
+    canvas1.height = canvas2.height = height;
+
     let onlinelist = {};
     let socket = io();
 
@@ -111,6 +129,7 @@
     document.onclick = function(e){
         // On join button click
         if (e.target.classList.contains('join')){
+            // Event propogation -- find lobbyid corresponding to join button pressed
             let lobbyid = e.target.parentNode.previousElementSibling.innerHTML;
             socket.emit('lobbyjoin', lobbyid);
         }
@@ -146,6 +165,10 @@
         }
     });
 
+    document.getElementById('lobbyDiv-teambutton').onclick = function (){
+        socket.emit('change team');
+    }
+
     document.getElementById('lobbyDiv-leavebutton').onclick = function (){
         socket.emit('lobbyleave');
     }
@@ -163,4 +186,95 @@
     document.getElementById('lobbyDiv-startbutton').onclick = function (){
         socket.emit('startgame');
     }
+
+    socket.on('startgame response', function(approved){
+        if(approved){
+            // If approved, bring socket to game
+            document.getElementById('lobbyDiv').style.display = 'none';
+            document.getElementById('lobbyDiv-host').style.display = 'none';
+            document.getElementById('gameDiv').style.display = 'inline-block';
+        }
+        else alert("you did something wrong ");
+    });
+
+    socket.on('update', function(pack){
+        if(pack["1"].hasOwnProperty(socket.id)){
+            render(ctx1, pack["1"]); // My screen
+            render(ctx2, pack["2"]); // Opponent's
+        }
+        else if(pack["2"].hasOwnProperty(socket.id)){
+            render(ctx1, pack["2"]); // My screen
+            render(ctx2, pack["1"]); // Opponent's
+        }
+        else (alert("Something went terribly wrong."))
+    });
+
+    //-------------------------------------------------------------------------
+    // GAME FUNCTIONS
+    //-------------------------------------------------------------------------
+
+    function t2p(t)     { return t*TILE;                     }; // tile to point
+    function p2t(p)     { return Math.floor(p/TILE);         }; // point to tile
+    function tformula(tx,ty) {return tx + (ty*SIZE.tw)       }; // tile to array index
+    function pformula(x,y)   {return tformula(p2t(x),p2t(y)) }; // point to array index
+
+    function render(ctx, pack){
+        ctx.clearRect(0, 0, width, height);
+        for (i in pack){
+            if (i === 'map'){
+                // Render map
+                for(let y = 0 ; y < SIZE.th ; y++) {
+                    for(let x = 0 ; x < SIZE.tw ; x++) {
+                        let cell = pack[i][tformula(x,y)];;
+                        if (cell){
+                            if (cell == BLOCKS.BEDROCK) ctx.fillStyle = COLOR.BLACK;
+                            else if (cell == BLOCKS.GOAL) ctx.fillStyle = COLOR.PURPLE;
+                            else if (cell == BLOCKS.BRICK) ctx.fillStyle = COLOR.BRICK;
+                            else if (cell == BLOCKS.SPAWN) ctx.fillStyle = COLOR.WHITE;
+                            ctx.fillRect(x * TILE, y * TILE, TILE, TILE);
+                        }
+                    }
+                }
+            }
+            else{ // Render players
+                ctx.fillStyle = COLOR.PINK;
+                ctx.fillRect(pack[i].x, pack[i].y, TILE, TILE);
+            }
+        }
+    }
+
+    // function renderFinish(){
+    //     ctx.rect(width/2,height/2,width/2,height/2);
+    //     ctx.fillStyle = "rgba(155,155,155,0.8)";
+    //     ctx.fill();
+    //     ctx.font = "30px Arial";
+    //     ctx.fillStyle = "pink";
+    //     ctx.textAlign = "center";
+    //     ctx.fillText(`FINISHUHH \n Time : ${formatTime(finishTime)}`, width/2,height/2);
+    // }
+
+    function keycontrols(ev, down) {
+        switch(ev.keyCode) {
+            // movement
+            case KEY.LEFT: ev.preventDefault(); socket.emit('keyPress', {inputId:'left', state: down}); break;
+            case KEY.A: socket.emit('keyPress', {inputId:'left', state: down}); break;
+            case KEY.RIGHT: ev.preventDefault(); socket.emit('keyPress', {inputId:'right', state: down}); break;
+            case KEY.D: socket.emit('keyPress', {inputId:'right', state: down}); break;
+            case KEY.UP: ev.preventDefault(); socket.emit('keyPress', {inputId:'up', state: down}); break;
+            case KEY.SPACE: ev.preventDefault(); socket.emit('keyPress', {inputId:'up', state: down}); break;
+            case KEY.W: socket.emit('keyPress', {inputId:'up', state: down}); break;
+        }
+    }
+
+    // EVENTS
+    document.addEventListener("keydown", function (ev){ keycontrols(ev,true);}, false);
+    document.addEventListener('keyup', function(ev) { keycontrols(ev,false);}, false);
+    canvas1.addEventListener("mousedown", function(ev){
+        // Make the click the tip of the cursor, not middle
+        let x = ev.clientX+MOUSE_OFFSET.x;
+        let y = ev.clientY+MOUSE_OFFSET.y;
+
+        // Ask to place block
+        socket.emit('keyPress', {inputId:'mouse',state:{x: x,y: y}});
+    }, false);
 })();
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /*
 	TODO:
 	- Prevent exploits : signin, create requests (all buttons basically)
-	- Remove redundant lists : client, lobby
-	- Deal with idling : https://stackoverflow.com/questions/48472977/how-to-catch-and-deal-with-websocket-is-already-in-closing-or-closed-state-in
+	- Deal with idling : 
+	https://stackoverflow.com/questions/48472977/how-to-catch-and-deal
+	-with-websocket-is-already-in-closing-or-closed-state-in
 	- Start game
-
+	----------> l 126
 */
 
 let express = require('express');
@@ -18,14 +19,9 @@ app.get('/',function(req, res) {
 app.use('/client',express.static(__dirname + '/client'));
 
 server.listen(process.env.PORT || 2000);
-console.log("Server started.");
 
-let onlinelist = {};
-let game_list = {};
-
-//-------------------------------------------------------------------------
-// LOBBY
-//-------------------------------------------------------------------------
+let onlinelist = {}; //Players who have signed in
+let gamelist = {};
 
 io.on('connection', function(socket){
 	// Leave default room, we only want a socket to be in 1 at a time
@@ -44,8 +40,6 @@ io.on('connection', function(socket){
 			// Allow client into home menu
 			socket.emit('signin approval', true);
 
-			console.log(io.sockets.adapter.rooms);
-
 			// Update client's lobby list
 			socket.emit('lobbylist change', io.sockets.adapter.rooms);
 
@@ -58,8 +52,8 @@ io.on('connection', function(socket){
 	});
 	
 	socket.on('lobbycreate', function(settings){
-		// Check lobby settings
-		if(typeof settings.name === 'string' && settings.name.length >= 4){
+		// Check if player isn't already in a lobby, and lobby settings
+		if(!Object.keys(socket.rooms)[0] && typeof settings.name === 'string' && settings.name.length >= 4){
 			// Create unique lobby id
 			let lobbyid = (Math.random()+1).toString(36).slice(2, 18);
 
@@ -116,8 +110,8 @@ io.on('connection', function(socket){
 		if(lobbyid){
 			// Get array of players in room
 			let players = Object.keys(io.sockets.adapter.rooms[lobbyid].sockets);
-
-			// Get number of players left after current socket leaves (false if empty)
+			
+			// Get number of players left after current socket leaves (0 = false if empty)
 			let number = players.length - 1;
 			
 			// If players still remain in lobby
@@ -126,9 +120,23 @@ io.on('connection', function(socket){
 				if (io.sockets.adapter.rooms[lobbyid].host === socket.id){
 					// Give host to the next player in lobby (0th index is the socket that left -- list not updated)
 					io.sockets.adapter.rooms[lobbyid].host = players[1];
+					
+					// Allow client into lobby with host priviledge, but only if game isn't happening
+					if (!gamelist[lobbyid]) io.sockets.connected[players[1]].emit('lobbycreate approval', true);
+				}
+				// If game was being played
+				if (gamelist[lobbyid]){
+					// Remove from game's player list
+					let index = gamelist[lobbyid].players.indexOf(socket.id);
+					gamelist[lobbyid].players.splice(index, 1);
 
-					// Allow client into lobby with host priviledge
-					io.sockets[players[1]].emit('lobbycreate approval', true);
+					// Remove from team's player list
+					index = gamelist[lobbyid].teams[socket.team].players.indexOf(socket.id);
+					gamelist[lobbyid].teams[socket.team].players.splice(index, 1);
+
+					// Delete game elements
+					delete socket.character;
+					delete socket.team;
 				}
 				socket.leave(lobbyid);
 
@@ -140,9 +148,17 @@ io.on('connection', function(socket){
 				// Room deletes automatically when last player leaves
 				socket.leave(lobbyid);
 
+				// Delete game elements if game was being played
+				if (gamelist[lobbyid]){
+					delete socket.character;
+					delete socket.team;
+					delete gamelist[lobbyid];
+				}
+
 				// Update lobby list in Home
 				io.emit('lobbylist change', io.sockets.adapter.rooms);
 			}
+
 			// Bring socket back to Home
 			socket.emit('lobbyleave response', true);
 
@@ -156,20 +172,60 @@ io.on('connection', function(socket){
 		// Get lobbyid of player
 		let lobbyid = Object.keys(socket.rooms)[0];
 
-		// If lobby exists and if the socket is host, create and start game
-		if(lobbyid && io.sockets.adapter.rooms[lobbyid].host === socket.id){
-			game_list[lobbyid] = new Game();
+		// If lobby exists, there's enough players and socket is host
+		if (lobbyid && Object.keys(io.sockets.adapter.rooms[lobbyid].sockets).length > 1
+			&& io.sockets.adapter.rooms[lobbyid].host === socket.id){
+				// Create and start game
+				gamelist[lobbyid] = new Game(lobbyid);
+				io.in(lobbyid).emit('startgame response', true);
 
-		} else socket.emit('startgame response', false);
+			} else socket.emit('startgame response', false);
+		});
+
+	socket.on('keyPress',function(data){
+		// Get lobbyid of player
+		let lobbyid = Object.keys(socket.rooms)[0];
+
+		// If player is in lobby and the game exists
+		if(gamelist[lobbyid] && socket.hasOwnProperty('character')){
+			if(data.inputId === 'left')
+				socket.character.left = data.state;
+			else if(data.inputId === 'right')
+				socket.character.right = data.state;
+			else if(data.inputId === 'up')
+				socket.character.up = data.state;
+			else if (data.inputId === 'mouse')
+				gamelist[lobbyid].teams[socket.team].newBlock(data.state.x, data.state.y);
+		}
 	});
 });
 
 //-------------------------------------------------------------------------
-// CONSTANTS
+// UPDATE LOOP
+//-------------------------------------------------------------------------
+
+let lastUpdateTime = (new Date()).getTime();
+// Starts game loop, which constantly emits 'update' to clients
+setInterval(function() {
+	let currentTime = (new Date()).getTime();
+	let dt = (currentTime - lastUpdateTime)/1000;
+
+	// For each game, update each player and add their info to packet to send to clients
+	for (let id in gamelist){
+		gamelist[id].update(dt);
+		io.in(id).emit('update', gamelist[id].getUpdatePack());
+	}
+	
+	lastUpdateTime = currentTime;
+}, 1000/60);
+
+//-------------------------------------------------------------------------
+// CONSTANTS AND UTILITY FUNCTIONS
 //-------------------------------------------------------------------------
 
 const SIZE   = { tw: 30, th: 30};
 	TILE     = 20;
+	HEIGHT 	 = SIZE.th*TILE;
 	GRAVITY  = 9.8 * 6; // default (exagerated) gravity
 	MAXDX    = 15;      // default max horizontal speed (15 tiles per second)
 	MAXDY    = 60;      // default max vertical speed   (60 tiles per second)
@@ -177,48 +233,11 @@ const SIZE   = { tw: 30, th: 30};
 	FRICTION = 1/6;     // default take 1/6 second to stop from maxdx (horizontal friction)
 	IMPULSE  = 1500;    // default player jump impulse
 	BUILD_TIME = 5;     // default time allowed for build phase
+	AMOUNT_TEAMS = 2;
 	BLOCKS   = { NULL: 0, SPAWN: 1, GOAL: 2, BEDROCK: 3,  BRICK: 4, WOOD: 5 };
 	COLLIDER_BLOCKS = [BLOCKS.BEDROCK, BLOCKS.BRICK, BLOCKS.WOOD];
 	IMMUTABLE_BLOCKS = [BLOCKS.BEDROCK, BLOCKS.SPAWN, BLOCKS.GOAL];
-
-let fps      = 60;
-	step     = 1/fps;
-	width    = SIZE.tw * TILE;
-	height   = SIZE.th * TILE;
-	player   = {};
-	timer_deadline = null; // Date when the timer runs out
-	timer = 0;
-	finishTime = 0;
-	gameState = GAMESTATES.BUILD;
-	player_list = {};
-	map = [];
-
-//-------------------------------------------------------------------------
-// GAME FUNCTIONS
-//-------------------------------------------------------------------------
-function t2p(t)     { return t*TILE;                     }; // tile to point
-function p2t(p)     { return Math.floor(p/TILE);         }; // point to tile
-function tformula(tx,ty) {return tx + (ty*SIZE.tw)       }; // tile to array index
-function pformula(x,y)   {return tformula(p2t(x),p2t(y)) }; // point to array index
-function tcell(tx,ty) { return cells[tformula(tx,ty)];   }; // get cell with tile from array
-function pcell(x,y)   { return tcell(p2t(x),p2t(y));     }; // get cell with point from array
-
-function isSurroundingCellTraversable(tx,ty){
-	cell = tcell(tx,ty);
-	for (block of COLLIDER_BLOCKS) if(cell === block) return true;
-	return false;
-}
-
-function newBlock(x,y){
-	let cell = pcell(x,y);
-	if (cell == BLOCKS.NULL){ // add block
-		cells[pformula(x,y)] = BLOCKS.BRICK;
-	}
-	else { // delete
-		if (!IMMUTABLE_BLOCKS.includes(cell))
-			cells[pformula(x,y)] = BLOCKS.NULL;
-	}
-}  
+	PHASES = { BUILD: 0, RACE: 1, FINISH: 2};
 
 function setTimer(diff = 0){
 	if (gameState === GAMESTATES.RACE)
@@ -227,18 +246,137 @@ function setTimer(diff = 0){
 		timer_deadline = new Date(new Date().getTime() + diff*60000);
 }
 
-function resetPlayer(player) {
-	player.x = player.start.x;
-	player.y = player.start.y;
-	player.dx = player.dy = 0;
+function bound(x, min, max) {
+	return Math.max(min, Math.min(max, x));
+}
+function t2p(t)     { return t*TILE;                     }; // tile to point
+function p2t(p)     { return Math.floor(p/TILE);         }; // point to tile
+function tformula(tx,ty) {return tx + (ty*SIZE.tw)       }; // tile to array index
+function pformula(x,y)   {return tformula(p2t(x),p2t(y)) }; // point to array index
+
+function tcell(map,tx,ty) {return map[tformula(tx,ty)];}; // get cell with tile from array
+
+function isSurroundingCellTraversable(map,tx,ty){
+	cell = tcell(map,tx,ty);
+	for (block of COLLIDER_BLOCKS) if(cell === block) return true;
+	return false;
+}
+
+function getRandomInt(min, max) { //min and max included
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+function newMap(){
+	let map = [];
+	// SETUP MAP
+	for(let i = 0; i < SIZE.tw*SIZE.th; i++){
+		map[i] = BLOCKS.NULL;// all to 0
+	}
+	  // Walls
+	for(let i = 0; i< SIZE.th*SIZE.tw; i+=SIZE.tw){
+		map[i] = BLOCKS.BEDROCK; // vertical
+		map[SIZE.tw+i-1] = BLOCKS.BEDROCK;
+	}
+	for(let i = 0; i < SIZE.tw; i++){
+		map[i] = BLOCKS.BEDROCK; // horizontal
+		map[SIZE.tw*(SIZE.th-1)+i] = BLOCKS.BEDROCK;
+	}
+	  // Spawn
+	map[tformula(1,SIZE.th-3)] = BLOCKS.SPAWN;
+	map[tformula(1,SIZE.th-2)] = BLOCKS.SPAWN;
+	map[tformula(2,SIZE.th-3)] = BLOCKS.SPAWN;
+	map[tformula(2,SIZE.th-2)] = BLOCKS.SPAWN;
+	  // Goal
+	let randX = getRandomInt(1, SIZE.tw-2);
+	let randY = getRandomInt(2, SIZE.th-2);
+	while(map[tformula(randX,randY)]){ // while possible goal locations are already occupied
+		randX = getRandomInt(1, SIZE.tw-2);
+		randY = getRandomInt(2, SIZE.th-2);
+	}
+	map[tformula(randX,randY)] = BLOCKS.GOAL;
+	map[tformula(randX,randY-1)] = BLOCKS.GOAL;
+
+	return map;
 }
 
 //-------------------------------------------------------------------------
 // OBJECTS
 //-------------------------------------------------------------------------
 
-function Player(id){
-	this.start    = { x: TILE, y: height-2*TILE};
+function Game(lobbyid){
+	// Hold keys to lobby and players
+	this.lobbyid = lobbyid;
+	this.players = Object.keys(io.sockets.adapter.rooms[lobbyid].sockets);
+
+	// Create Team Objects
+	this.teams = {};
+	for (let i = 1; i<=AMOUNT_TEAMS; i++){
+		this.teams[`${i}`] = new Team();
+	}
+	// Create Player Objects and add to different teams
+	let t = 1;
+	this.players.forEach(p => {
+		io.sockets.connected[p].character = new Character(); // Attach Character object to socket
+		io.sockets.connected[p].team = t; // Attach team to socket
+		if (t>AMOUNT_TEAMS) t=1;
+		this.teams[t].players.push(p);
+		t++;
+	});
+
+	this.fps = 60;
+	this.phase = PHASES.BUILD;
+
+	// Update each player
+	this.update = function(dt){
+		this.players.forEach(p => {
+			// Get team that player is in
+			let t = io.sockets.connected[p].team;
+
+			// Update player location using map for that team
+			io.sockets.connected[p].character.update(this.teams[t].map,dt);
+		});
+	}
+
+	this.getUpdatePack = function(){
+		let updatepack = {};
+		for (let t in this.teams){
+			// Make updatepack for team
+			updatepack[t] = {};
+
+			// Add map to team updatepack object
+			updatepack[t]['map'] = this.teams[t].map;
+
+			// For each player in team, add player's updatepack to team's updatepack
+			this.teams[t].players.forEach(p => {
+				updatepack[t][p] = io.sockets.connected[p].character.getUpdatePack();
+			});
+		}
+		return updatepack;
+	}
+	// timer_deadline = null; // Date when the timer runs out
+	// timer = 0;
+	// finishTime = 0;
+}
+
+function Team(){
+	this.map = newMap();
+	this.players = [];
+	this.newBlock = function(x,y){
+		let cell = tcell(this.map,p2t(x),p2t(y));
+
+		// If no block there, add block
+		if (cell == BLOCKS.NULL)
+			this.map[pformula(x,y)] = BLOCKS.BRICK;
+
+		// Otherwise check if deletable, then delete it
+		else {
+			if (!IMMUTABLE_BLOCKS.includes(cell))
+				this.map[pformula(x,y)] = BLOCKS.NULL;
+		}
+	}
+}
+
+function Character(){
+	this.start    = { x: TILE, y: HEIGHT-2*TILE};
 	this.x        = this.start.x;
 	this.y        = this.start.y;
 	this.dx       = 0;
@@ -257,13 +395,20 @@ function Player(id){
 	this.jumping  = false;
 	this.falling  = false;
 
-	this.update = function(dt){
-	   	/* all collision is done using the top left corner of the player; */
-		let wasleft    = entity.dx  < 0,
-			wasright   = entity.dx  > 0,
-			friction   = this.friction * (this.falling ? 0.5 : 1),
-			accel      = this.accel    * (this.falling ? 0.5 : 1);
-		
+	this.getUpdatePack = function(){
+		return {
+			x: this.x,
+			y: this.y,
+		};
+	}
+
+	this.update = function(map,dt){
+		/* all collision is done using the top left corner of the player; */
+		let wasleft= this.dx  < 0,
+		wasright   = this.dx  > 0,
+		friction   = this.friction,
+		accel      = this.accel;
+	
 		// GENERAL MOVEMENT
 		this.ddx = 0;
 		this.ddy = this.gravity;
@@ -287,14 +432,14 @@ function Player(id){
 			this.dx = 0; // clamp at zero to prevent friction from making us jiggle side to side
 
 			// update variables #1
-		let tx = p2t(this.x); // this tile position
+		let tx = p2t(this.x); // player tile position
 		let ty = p2t(this.y);
 		let nx = this.x%TILE; // overlap on tile (remainder)
-		let ny = this.y%TILE; // y overlap on grid  
-		let blockhere = isSurroundingCellTraversable(tx,ty) // Get surrounding cells around this
-		let blockright = isSurroundingCellTraversable(tx+1,     ty);
-		let blockbelow = isSurroundingCellTraversable(tx,     ty+1);
-		let blockbelow_right = isSurroundingCellTraversable(tx+1,     ty+1);
+		let ny = this.y%TILE; // y overlap on grid
+		let blockhere = isSurroundingCellTraversable(map,tx,ty) // Get surrounding cells around this
+		let blockright = isSurroundingCellTraversable(map,tx+1,ty);
+		let blockbelow = isSurroundingCellTraversable(map,tx,ty+1);
+		let blockbelow_right = isSurroundingCellTraversable(map,tx+1,ty+1);
 
 			// x collision
 		if (this.dx > 0) { // moving right 
@@ -316,14 +461,14 @@ function Player(id){
 		this.dy = bound(this.dy + (dt * this.ddy), -this.maxdy, this.maxdy);
 
 			// update variables #2
-		tx = p2t(this.x); // this tile position
+		tx = p2t(this.x); // p tile position
 		ty = p2t(this.y);
 		nx = this.x%TILE; // overlap on tile (remainder)
 		ny = this.y%TILE; // y overlap on grid
-		blockhere = isSurroundingCellTraversable(tx,ty);
-		blockright = isSurroundingCellTraversable(tx+1,ty);
-		blockbelow = isSurroundingCellTraversable(tx,ty+1);
-		blockbelow_right = isSurroundingCellTraversable(tx+1,ty+1);
+		blockhere = isSurroundingCellTraversable(map,tx,ty);
+		blockright = isSurroundingCellTraversable(map,tx+1,ty);
+		blockbelow = isSurroundingCellTraversable(map, tx,ty+1);
+		blockbelow_right = isSurroundingCellTraversable(map, tx+1,ty+1);
 
 			// y collision
 		if (this.dy > 0) { // falling
@@ -339,75 +484,35 @@ function Player(id){
 			if ((blockhere && !blockbelow) || (blockright && !blockbelow_right && nx)) {
 				this.y  = t2p(ty + 1);
 				this.dy = 0;
-				ny        = 0;
+				ny   = 0;
 			}
 		}
 
 		// FINISH CHECK 
-		tx = p2t(entity.x); // update variables #3
-		ty = p2t(entity.y);
-		nx = entity.x%TILE;
-		ny = entity.x%TILE;
-		finishhere = (tcell(tx,ty) === BLOCKS.GOAL);
-		finishright = (tcell(tx+1,ty) === BLOCKS.GOAL);
-		finishbelow = (tcell(tx+1,ty+1) === BLOCKS.GOAL);
+		tx = p2t(this.x); // update variables #3
+		ty = p2t(this.y);
+		nx = this.x%TILE;
+		ny = this.x%TILE;
+		finishhere = (tcell(map,tx,ty) === BLOCKS.GOAL);
+		finishright = (tcell(map,tx+1,ty) === BLOCKS.GOAL);
+		finishbelow = (tcell(map,tx+1,ty+1) === BLOCKS.GOAL);
 
 			// finish collision
-		if (finishhere || (finishright && nx) || (finishbelow && ny)){ // check for goal
-			if (gameState == GAMESTATES.RACE){
-				gameState = GAMESTATES.FINISH;
-				finishTime = timer;
-				clearTimer();
-				return;
-			}
-		}
+		// if (finishhere || (finishright && nx) || (finishbelow && ny)){ // check for goal
+		// 	if (gameState == GAMESTATES.RACE){
+		// 		gameState = GAMESTATES.FINISH;
+		// 		finishTime = timer;
+		// 		clearTimer();
+		// 		return;
+		// 	}
+		// }
 
-		entity.falling = ! (blockbelow || (nx && blockbelow_right));
-	}
-
-	this.getUpdatePack = function(){
-		return {
-			id: this.id,
-			x: this.x,
-			y: this.y,
-		};
+		this.falling = ! (blockbelow || (nx && blockbelow_right));
 	}
 
-	socket.on('keyPress',function(data){
-		if(data.inputId === 'left')
-			this.left = data.state;
-		else if(data.inputId === 'right')
-			this.right = data.state;
-		else if(data.inputId === 'up')
-			this.up = data.state;
-	});
-}
-function setupMap(){
-	// SETUP MAP
-	for(let i = 0; i < SIZE.tw*SIZE.th; i++){
-	  map[i] = BLOCKS.NULL;// all to 0
+	this.reset = function(){
+		this.x = this.start.x;
+		this.y = this.start.y;
+		this.dx = this.dy = 0;
 	}
-	  // Walls
-	for(let i = 0; i< SIZE.th*SIZE.tw; i+=SIZE.tw){
-	  map[i] = BLOCKS.BEDROCK; // vertical
-	  map[SIZE.tw+i-1] = BLOCKS.BEDROCK;
-	}
-	for(let i = 0; i < SIZE.tw; i++){
-	  map[i] = BLOCKS.BEDROCK; // horizontal
-	  map[SIZE.tw*(SIZE.th-1)+i] = BLOCKS.BEDROCK;
-	}
-	  // Spawn
-	map[tformula(1,SIZE.th-3)] = BLOCKS.SPAWN;
-	map[tformula(1,SIZE.th-2)] = BLOCKS.SPAWN;
-	map[tformula(2,SIZE.th-3)] = BLOCKS.SPAWN;
-	map[tformula(2,SIZE.th-2)] = BLOCKS.SPAWN;
-	  // Goal
-	let randX = getRandomInt(1, SIZE.tw-2);
-	let randY = getRandomInt(2, SIZE.th-2);
-	while(tcell(randX,randY)){ // while possible goal locations are already occupied
-	  randX = getRandomInt(1, SIZE.tw-2);
-	  randY = getRandomInt(2, SIZE.th-2);
-	}
-	map[tformula(randX,randY)] = BLOCKS.GOAL;
-	map[tformula(randX,randY-1)] = BLOCKS.GOAL;
 }
